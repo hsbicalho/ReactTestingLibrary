32a8c08deb9efac91656bf9785d9289f2fa0cc35
hsbicalho <hsbicalho@gmail.com>
2022-05-10 16:55:58 -0300

    requisitos 4 e 5

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('test NotFound', () => {
+  it('Test if exist an h2, with message requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const notFound = screen.getByRole(
+      'heading', { level: 2, name: /page requested not found/i },
+    );
+    expect(notFound).toBeInTheDocument();
+  });
+  it('test if shows the required image', () => {
+    renderWithRouter(<NotFound />);
+    const imgNotFound = screen.getByRole('img', { name: /pikachu crying/i });
+    expect(imgNotFound).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,54 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import pokemons from '../data';
+import App from '../App';
+
+const nextPokemon = pokemons.filter((_, i) => i !== 0);
+describe('teste o componente Pokedex', () => {
+  it('test if exist an h2 with the required message', () => {
+    renderWithRouter(<App />);
+    const head = screen.getByRole('heading', { level: 2, name: /encountered pokémon/i });
+    expect(head).toBeInTheDocument();
+  });
+  it('test if navegate by pokémons', () => {
+    renderWithRouter(<App />);
+    const currPoke = screen.getByRole('button', { name: /próximo pokémon/i });
+    expect(currPoke).toBeInTheDocument();
+    nextPokemon.forEach((poke, i) => {
+      userEvent.click(currPoke);
+      expect(screen.getByText(poke.name)).toBeInTheDocument();
+      expect(screen.queryByText(pokemons[i].name)).not.toBeInTheDocument();
+    });
+    userEvent.click(currPoke);
+    expect(screen.queryByText(pokemons[0].name)).toBeInTheDocument();
+  });
+  it('Teste se a Pokédex tem os botões de filtro', () => {
+    renderWithRouter(<App />);
+    const types = ['Electric', 'Fire', 'Bug',
+      'Poison', 'Psychic', 'Normal', 'Dragon'];
+    types.forEach((type) => {
+      expect(screen.getByRole('button', { name: type })).toBeInTheDocument();
+    });
+    const magicNumber = 7;
+    const typesBtns = screen.getAllByTestId('pokemon-type-button');
+    expect(typesBtns).toHaveLength(magicNumber);
+  });
+  it('test if have a button to reset filter', () => {
+    renderWithRouter(<App />);
+    const allPoke = screen.getByRole('button', { name: /all/i });
+    expect(allPoke).toBeInTheDocument();
+    expect(allPoke).toHaveTextContent('All');
+    expect(screen.getByTestId('pokemon-name')).toBeInTheDocument();
+    const buttonNext = screen.getByRole('button', { name: /próximo pokémon/i });
+    userEvent.click(buttonNext);
+    const charmander = screen.getByText('Charmander');
+    const pikachu1 = screen.queryByText('Pikachu');
+    expect(charmander).toBeInTheDocument();
+    expect(pikachu1).not.toBeInTheDocument();
+    userEvent.click(allPoke);
+    const pikachu2 = screen.getByText('Pikachu');
+    expect(pikachu2).toBeInTheDocument();
+  });
+});
